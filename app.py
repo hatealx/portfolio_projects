77694c3269261a6f25d8a978cdb68ecee52f642d
abel Atsou <aatsou12@gmail.com>
2024-02-19 17:38:16 +0000

    added landing and upadate footer and header of each page

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 import bcrypt
-import sqlite3
+from datetime  import  datetime
+
 task_app = Flask(__name__)
 task_app.secret_key='my_key'
 
@@ -17,6 +18,7 @@ class Users(db.Model):
     name = db.Column(db.String(200), nullable=False, unique=True)
     email= db.Column(db.String(40), nullable=False)
     password = db.Column(db.String(100), nullable=False)
+   
     
 
     def __init__(self, name, email, password):
@@ -37,6 +39,7 @@ class Tasks(db.Model):
     name = db.Column(db.String(200), nullable=False,)
     status = db.Column(db.String(10), nullable=False, default=0)
     user_name = db.Column(db.String(200), nullable=False, unique=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def to_dict(self):
         return {
@@ -44,6 +47,7 @@ class Tasks(db.Model):
             'name': self.name,
             'status': self.status,
             'user_name': self.user_name,
+            'created_at': self.created_at.strftime('%Y-%m-%d')
         }
 
 
@@ -59,6 +63,10 @@ def add_header(response):
 #deining my apps routes 
 @task_app.route('/', methods=['GET', 'POST'])
 def index():
+    return render_template('landing.html')
+
+@task_app.route('/login', methods=['GET', 'POST'])
+def login():
     login_error = None
     if "name" in session:
         return redirect(url_for('mytasks'))
@@ -73,31 +81,32 @@ def index():
             # Use bcrypt to check if the provided password matches the stored hashed password
             if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
                 session["name"]=user.name
-
+                #if the user has successfully
+                    #get all the tasks created by user  
                 all_tasks_objects = Tasks.query.filter_by(user_name=session["name"]).all()
+                    #store each task information in a dictionary and all the tasks in a all_tasks_dictionnary
                 all_tasks_dictionnary =  [task.to_dict() for task in all_tasks_objects]
-                print()
-
-                
+                    #create a session and store all the dictionary inside it 
                 session["tasks"]=all_tasks_dictionnary
-
+                    #redirect user to tasks dashboard 
                 return redirect(url_for('mytasks'))
             else:
+                #if the name is correct but the password is wrong prints a login error
                 login_error = "Incorrect UserName or Password"
 
         else:
+            #if user_name does not  exist in the database tell the user to create an account
             login_error = "You are not registered yet please sign up"
-
     return render_template('index.html', login_error=login_error)
 
 @task_app.route('/register',methods=['GET', 'POST'] )
 def register():
     error_message = None
+    #getting form information if there is post request if there is post request
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email'] 
         password = request.form['password']
-
 
         existing_user = Users.query.filter_by(name=name).first()
 
@@ -108,7 +117,7 @@ def register():
             new_user = Users(name=name, password=password, email=email)
             db.session().add(new_user)
             db.session().commit()
-            return redirect('/')
+            return redirect('/logout')
     print("register")
     return render_template('register.html', error_message = error_message)
 
@@ -121,7 +130,7 @@ def mytasks():
             
     if "name" in session:
         if request.method == 'POST':
-            task_name = request.form['task_name']
+            task_name = request.form['task_name'].capitalize()
             new_task = Tasks(name=task_name, user_name=session['name'])
             db.session().add(new_task)
             db.session().commit()
@@ -133,9 +142,9 @@ def mytasks():
         print(session["tasks"])
         return render_template('mytasks.html', user=session["name"], my_tasks=session["tasks"])
     else:
-        return redirect(url_for("index"))
+        return redirect(url_for("login"))
 
-
+#endpoint for updtating tasks status 
 @task_app.route('/update_status', methods=['GET', 'POST'])
 def task_status():
     status = request.form.get('status')
@@ -150,7 +159,7 @@ def task_status():
         all_tasks_dictionnary =  [task.to_dict() for task in all_tasks_objects]
         session["tasks"] = all_tasks_dictionnary
 
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
     
 
 
@@ -159,9 +168,41 @@ def task_status():
 @task_app.route('/logout', methods=['GET', 'POST'])
 def logout():
     session.pop('name', None)
-    return redirect(url_for("index")) 
+    return redirect(url_for("login")) 
 
-    
+
+@task_app.route('/delete/<int:id>')
+def delete(id):
+    #delete  a task based on its id 
+    task_to_delete = Tasks.query.get(id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    #updating the session 
+    all_tasks_objects = Tasks.query.filter_by(user_name=session["name"]).all()
+    all_tasks_dictionnary =  [task.to_dict() for task in all_tasks_objects]
+    session["tasks"] = all_tasks_dictionnary
+
+
+    return redirect(url_for("mytasks"))
+
+@task_app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    print("###################")
+    task_to_edit = Tasks.query.get(int(id))
+    print(task_to_edit)
+    if request.method == 'POST':
+             print("###################")
+             task_to_edit.name = request.form["taskNamee"]
+    db.session.commit()
+
+    #updating the session 
+    all_tasks_objects = Tasks.query.filter_by(user_name=session["name"]).all()
+    all_tasks_dictionnary =  [task.to_dict() for task in all_tasks_objects]
+    session["tasks"] = all_tasks_dictionnary
+
+
+    return redirect(url_for("mytasks")) 
+
 
 
 
--- static/mytasks.js
+++ static/mytasks.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 window.onload = function() {
     var radios = document.querySelectorAll('input[type="radio"]');
     radios.forEach(function(radio) {
@@ -12,7 +18,7 @@ window.onload = function() {
             xhr.onload = function() {
                 if (xhr.status === 200) {
                     // Redirect to the /mytasks page after successful 
-                    window.location.reload(true);;
+                    window.location.reload(true);
                 } else {
                     console.error('Error updating status:', xhr.statusText);
                 }
@@ -20,5 +26,40 @@ window.onload = function() {
             xhr.send('status=' + newStatus + '&task_id=' + taskId);
         });
     });
+
+
+
+
+
+    // Get the popup
+var btns = document.querySelectorAll(".form");
+
+// Get the <a> element that opens the popu
+
+// Get the <span> element that closes the popup
+var span = document.getElementsByClassName("close")[0];
+
+var popup = document.getElementById("popupForm");
+// When the user clicks on <span> (x), close the popup
+span.onclick = function() {
+    popup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the popup, close it
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
+
+btns.forEach(function(btn) {
+    btn.onclick = function() {
+        popup.style.display = "block";
+        var link_id = btn.getAttribute('name-task-id') ;
+        var form = document.getElementById('updateForm')
+        form.setAttribute("action", link_id);
+    }
+});
+
 };
 
